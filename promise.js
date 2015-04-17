dcd64ac9d624c6f93924d4f4081889042ef6c465
treelite <c.xinle@gmail.com>
2015-04-17 11:35:18 +0800

    Del unused code

--- promise.js
+++ promise.js
@@ -5,24 +5,4 @@
 
 var Resolver = require('./src/promise');
 
-/**
- * Plugin for Rebas
- *
- * @public
- * @param {Object} app server
- */
-Resolver.rebas = function (app) {
-    var nextTick = this.nextTick;
-
-    // 重载异步函数
-    // 以保存请求上下文
-    this.nextTick = function (fn) {
-        var id = app.stashContext();
-        nextTick(function () {
-            app.revertContext(id);
-            fn();
-        });
-    };
-};
-
 module.exports = Resolver;
